1021cf3d9d47a4fcac1b4ce8f1a2005dd12f7af8
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-13 11:31:59 +0530

    Bank Exercise modfied

---
+++ BankExerciseBeginner/BankExerciseBeginner/Bank.cs
@@ -0,0 +1,35 @@
+﻿namespace Bank
+{
+    public class Bank
+    {
+        private  int BankAccountNumber { get; set; }
+        private  string AccountHoldersName { get; set; }
+        private float BankBalance { get; set; }
+
+        public Bank(int accountNumber, string name, float balance)
+        {
+            BankAccountNumber = accountNumber;
+            AccountHoldersName = name;
+            BankBalance = balance;
+        }
+
+        public float Desposit(float value)
+        {
+            BankBalance += value;
+            return BankBalance;
+        }
+
+        public float Withdraw(float value)
+        {
+            BankBalance -= value;
+            return BankBalance;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Account Holders Name : " + AccountHoldersName);
+            Console.WriteLine("Account Number : " + BankAccountNumber);
+            Console.WriteLine("Account Balance : " + BankBalance);
+        }
+    }
+}
---
+++ BankExerciseBeginner/BankExerciseBeginner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bank
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var b = new Bank(1234, "Ayush", 50000);
+
+            b.Desposit(500);
+            b.Withdraw(1000);
+            b.Display();
+        }
+    }
+}
