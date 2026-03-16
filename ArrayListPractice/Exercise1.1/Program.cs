1b45be09a57954c92f6da42d21c03ec55367933c
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-16 19:20:37 +0530

    Array List Practice Exercise1.1

---
+++ ArrayListPractice/Exercise1.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ArrayListPractice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            var likes = new List<string>();
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter name : ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    if (likes.Count == 0)
+                    {
+                        Console.WriteLine("0 Likes in Post");
+                        break;
+                    }
+                    else if (likes.Count == 1)
+                    {
+                        Console.WriteLine("{0} have liked the post", likes[0]);
+                        break;
+                    }
+                    else if (likes.Count == 2)
+                    {
+                        Console.WriteLine("{0} and {1} have liked the post.", likes[0], likes[1]);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0}, {1} and {2} other have liked the post", likes[0], likes[1], likes.Count - 2);
+                        break;
+                    }
+                }
+                likes.Add(input);
+                continue;
+
+            }
+
+        }
+    }
+}
+
+
+
+  
