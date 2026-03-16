89739f1b5cfc9b013a8483b34d73f5c1349e35ea
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-16 19:22:43 +0530

    Array List Practice Exercise1.4

---
+++ ArrayListPractice/Exercise1.4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+
+
+/*4- Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+ * The list of numbers may include duplicates. 
+ * Display the unique numbers that the user has entered.
+*/
+
+namespace ArrayListPractice
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> number = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number or quit to stop : ");
+                var input = Console.ReadLine();
+                if (!int.TryParse(input, out int n))
+                {
+                    if (input.ToLower() == "quit")
+                    {
+                        Console.WriteLine("Enter Numbers are:");
+                        foreach (var num in number)
+                        {
+                            Console.WriteLine(num);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Try again");
+                        continue;
+                    }
+                }
+                else if (number.Contains(n))
+                {
+                    continue;
+                }
+
+                number.Add(n);
+                continue;
+
+            }
+            
+        }
+    } 
+}
