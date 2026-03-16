e5b69d2984a37250976c6a1b79e399f9ad90bdb6
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-16 19:21:06 +0530

    Array List Practice Exercise1.2

---
+++ ArrayListPractice/Exercise1.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ArrayListPractice
+{
+    /*2- Write a program and ask the user to enter their name. 
+     * Use an array to reverse the name and then store the result in a new string. 
+     * Display the reversed name on the console.
+     */
+
+
+    public class Program
+    {
+        public static void Main(string[] args) {
+
+            
+            while (true)
+            {
+                Console.WriteLine("Enter your name :");
+                var input = Console.ReadLine();
+                List<char> list = [];
+             
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Invalid name");
+                    break;
+
+                }
+                
+                
+                   
+                for(int i = input.Length-1; i>=0 ; i--)
+                {
+                    
+                    list.Add(input[i]);     
+                }
+                var result = string.Join("", list);
+               Console.WriteLine(result);
+
+                break;
+                
+            }
+        
+        }
+    }
+}
