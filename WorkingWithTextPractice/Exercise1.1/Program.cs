4c4a2ea53a4a9a404b380d2e877883c8913656a9
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-17 18:30:51 +0530

    Exercise 1.1

---
+++ WorkingWithTextPractice/Exercise1.1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace WorkingWithTextPractice
+{
+    public class Program
+    {
+
+        /*1- Write a program and ask the user to enter a few numbers separated by a hyphen. 
+         * Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", 
+         * display a message: "Consecutive"; otherwise, display "Not Consecutive".
+         */
+
+
+
+        public static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter numbers separated by hyphen : ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Invalid input. Exitting program");
+                    break;
+                }
+
+                var numList = input.Split("-")
+                                     .Select(n => int.Parse(n.Trim()))
+                                     .ToList();
+                bool flag = false;
+                for (int i = 0; i < numList.Count - 1; i++)
+                {
+                    if (numList[i + 1] == numList[i] + 1)
+                    {
+
+                        continue;
+                    }
+
+                    else if (numList[i + 1] == numList[i] - 1)
+                    {
+
+                        continue;
+                    }
+                    else { 
+                        flag = true;
+                        break; 
+                    }
+
+                }
+
+                if (!flag)
+                {
+                    Console.WriteLine("Consecutive");
+
+                }
+                else
+                {
+                    Console.WriteLine("Not Consecutive");
+                }
+            }
+
+        }
+    }
+}
