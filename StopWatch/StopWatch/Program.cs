2763fdf2b96bfe0d951a8a3f8b8029ea121d668a
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-19 20:16:09 +0530

    Stop Watch Exercise

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace StopWatch
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var watch = new StopWatch();
+            while (true)
+            {
+                Console.WriteLine("Enter s to start the watch or q to quit");
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Invalid input provided. Try again.");
+                    continue;
+                }
+                input = input.ToLower();
+                
+
+                if (input == "s")
+                {
+                    watch.StartWatch();
+                    Console.WriteLine("Watch is running. Enter e to stop : ");
+                    continue;
+   
+                }
+                else if(input == "e")
+                {
+                    watch.EndWatch();
+                    Console.WriteLine(watch.CalculateTime());
+                    continue;
+                }
+               
+                else if (input == "q")
+                {
+
+                    Console.WriteLine("Thanks for using the Watch.");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input . Try again");
+                    continue;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,32 @@
+﻿namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isRunning;
+
+        public void StartWatch()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Watch is already running");
+
+            _isRunning = true;
+            _startTime = DateTime.Now;
+        }
+
+        public void EndWatch()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("Watch is not running");
+
+            _isRunning = false;
+            _endTime = DateTime.Now;
+        }
+
+        public TimeSpan CalculateTime()
+        {
+            return _endTime - _startTime;
+        }
+    }
+}
