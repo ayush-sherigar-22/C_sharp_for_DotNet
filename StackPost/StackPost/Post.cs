438da28560e156637a98b21db69a129e17c24e1b
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-21 13:27:53 +0530

    Stack Post Exercise

---
+++ StackPost/StackPost/Post.cs
@@ -0,0 +1,64 @@
+﻿namespace StackPost
+{
+    public class Post
+    {
+
+        public string Title { get; }
+        public string Content { get; }
+        public DateTime Created { get; }
+        private int _upVote;
+        private int _downVote;
+
+        public Post(string title, string content)
+        {
+            if(string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentNullException("Title cant be empty");
+            }
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                throw new ArgumentNullException("Content cant be null");
+            }
+
+            Title = title;
+            Content = content;
+            Created = DateTime.Now;
+            _downVote = 0;
+            _upVote = 0;
+            DisplayPost();
+        }
+
+        public void Vote(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+               
+                    throw new InvalidOperationException("No such option available");            
+            }
+
+            if (input == "u")
+            {
+                _upVote++;
+                DisplayPost();
+            }
+            else if (input == "d")
+            {
+                _downVote++;
+                DisplayPost();
+
+            }
+            else
+            {
+                throw new InvalidOperationException("No such operation");
+            }
+           
+        }
+
+        public void DisplayPost()
+        {
+            Console.Clear();
+            Console.WriteLine($"Title : {Title} \t\t\t Created at {Created} \n\n {Content} \n\n\t Upvote : {_upVote} Downvote : {_downVote} ");
+        }
+
+    }
+}
---
+++ StackPost/StackPost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackPost
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Title: ");
+            var title = Console.ReadLine();
+
+            Console.Write("Enter Content: ");
+            var content = Console.ReadLine();
+
+            var post = new Post(title, content);
+
+            while (true)
+            {
+                Console.WriteLine("UpVopte(u) or DownVote(d) by or Quit(q) ");
+                var vote = Console.ReadLine();
+
+                if (vote == "q")
+                {
+                    break;
+                }
+
+                post.Vote(vote);
+                continue;
+            }
+
+        }
+    }
+}
