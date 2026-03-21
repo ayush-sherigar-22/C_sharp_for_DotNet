6b2919519f61f3e3827de5c802a3da3e61bcf28a
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-21 13:27:15 +0530

    Stack Exercise

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections;
+
+
+namespace StackExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+
+
+            var stack = new Stack();
+
+            while (true) {
+
+                Console.WriteLine("Chose one Option \n1.Push \n2.Pop \n3.Clear  : ");
+                var option = Console.ReadLine();
+                switch (int.Parse(option))
+                {
+                    case 1:
+                        Console.WriteLine("Enter object to push");
+                        var item = Console.ReadLine();
+                        stack.Push(item);
+                        continue;
+
+                    case 2:
+                        var obj = stack.Pop();
+                        Console.WriteLine($"Popped object is :{obj}");
+                        continue;
+
+                    case 3:
+                        stack.Clear();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        continue;
+                }
+            }
+            
+         }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,34 @@
+﻿namespace StackExercise
+{
+    public class Stack<T>
+    {
+        private readonly List<T> _stackList = [];
+
+        public void Push(T item)
+        {
+            if (item != null)
+            {
+                _stackList.Add(item);
+            }
+            else
+            {
+                throw new ArgumentNullException("Cannot add null object to Stack");
+            }
+        }
+
+        public T Pop() {
+
+            if(_stackList.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is Empty");
+            }
+            var index = _stackList.Count - 1;
+            var value = _stackList[index];
+            _stackList.RemoveAt(index);
+            return value;
+        }
+
+        public void Clear() => _stackList.Clear();
+
+    }
+}
