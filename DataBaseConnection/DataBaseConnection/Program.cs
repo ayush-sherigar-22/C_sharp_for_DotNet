b3be33d1182ca82b543846c8b8e9aa24cbc83ade
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-24 00:10:39 +0530

    Database Connection Exercise

---
+++ DataBaseConnection/DataBaseConnection/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Diagnostics;
+
+namespace DatabaseConnection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DbConnection sql = new SQLConnection("Server=.;Database=TestDb;");
+            sql.OpenConnection();
+            sql.CloseConnection();
+
+            DbConnection oracle = new OracleConnection("Server=.;Database=TestDb;");
+            oracle.OpenConnection();
+            oracle.CloseConnection();
+
+        }
+    }
+
+
+
+
+    public abstract class DbConnection
+    {
+        private readonly string _connectionString;
+        private readonly TimeSpan _timeOut;
+        private bool _status;
+        private static readonly TimeSpan _defaultTimeOut = TimeSpan.FromMilliseconds(10000);
+
+
+        protected DbConnection(String connectionstring)
+        {
+            if (String.IsNullOrWhiteSpace(connectionstring))
+            {
+                throw new ArgumentException("Invalid Connection String :");
+            }
+            _connectionString = connectionstring;
+            _timeOut = _defaultTimeOut;
+        }
+
+        protected DbConnection(String _connectionString, TimeSpan timeout)
+         : this(_connectionString)
+        {
+            if (timeout == TimeSpan.Zero)
+            {
+                throw new ArgumentException("Invalid Time provided");
+            }
+            _timeOut = timeout;
+
+        }
+
+
+
+        protected abstract void OpenCore();
+        protected abstract void CloseCore();
+
+
+        public void OpenConnection()
+        {
+
+            if (_status)
+            {
+                throw new InvalidOperationException("Connection is already open : ");
+
+            }
+            var connectionTime = Stopwatch.StartNew();
+
+
+            OpenCore();
+            connectionTime.Stop();
+
+            if (connectionTime.Elapsed > _timeOut)
+            {
+                throw new TimeoutException("Connection Timed out");
+            }
+            _status = true;
+        }
+        public void CloseConnection()
+        {
+            if (!_status)
+            {
+                throw new InvalidOperationException("No open connection.");
+            }
+
+            CloseCore();
+            _status = false;
+
+
+        }
+
+    }
+
+    public class SQLConnection(string _connectionstring) : DbConnection(_connectionstring)
+    {
+        protected override void CloseCore()
+        {
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Connection to SQL Database closed : ");
+        }
+
+        protected override void OpenCore()
+        {
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Connection to SQL database opened : ");
+        }
+    }
+
+    public class OracleConnection(string _connectionstring) : DbConnection(_connectionstring)
+    {
+
+        protected override void CloseCore()
+        {
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Connection to Oracle Database closed : ");
+        }
+
+        protected override void OpenCore()
+
+        {
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Connection to Oracle Database open");
+        }
+
+    }
+
+}
