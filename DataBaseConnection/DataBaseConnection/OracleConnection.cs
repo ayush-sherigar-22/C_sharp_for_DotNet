a2771315372a5669f54c078cbabc2d097db7b48d
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-03-24 17:52:23 +0530

    Database Connection Modified

---
+++ DataBaseConnection/DataBaseConnection/OracleConnection.cs
@@ -0,0 +1,23 @@
+﻿namespace DatabaseConnection
+{
+    public class OracleConnection(string _connectionstring) : DbConnection(_connectionstring)
+    {
+
+        protected override void CloseCore()
+        {
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Connection to Oracle Database closed : ");
+        }
+
+        protected override void OpenCore()
+
+        {
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Connection to Oracle Database open");
+        }
+
+    }
+
+}
---
+++ DataBaseConnection/DataBaseConnection/SQLConnection.cs
@@ -0,0 +1,20 @@
+﻿namespace DatabaseConnection
+{
+    public class SQLConnection(string _connectionstring) : DbConnection(_connectionstring)
+    {
+        protected override void CloseCore()
+        {
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Connection to SQL Database closed : ");
+        }
+
+        protected override void OpenCore()
+        {
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Connection to SQL database opened : ");
+        }
+    }
+
+}
