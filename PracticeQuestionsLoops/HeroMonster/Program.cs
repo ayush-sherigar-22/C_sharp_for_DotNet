f9dd272be1bf515290aef9a0bb42fa35744f1718
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-04-06 15:51:13 +0530

    MS Learn Hero Monster Game

---
+++ PracticeQuestionsLoops/HeroMonster/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HeroMonster
+{
+
+    public class Program
+    {
+        public static void Main(string[] args) {
+
+            var heroHealth = 10;
+            var monsterHealth = 10;
+            
+            var attackhealth=0;
+            do
+            {
+            
+                    attackhealth = new Random().Next(1, monsterHealth);
+                    monsterHealth -= attackhealth;
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Monster was attacked and lost {0} health and now has {1} health. ", attackhealth, monsterHealth);
+                   
+                
+                if(monsterHealth>=0)
+                {
+                    attackhealth = new Random().Next(1, heroHealth);
+                    heroHealth -= attackhealth;
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Hero was attacked and lost {0} health and now has {1} health. ", attackhealth, heroHealth);
+      
+                }
+
+
+
+            }while (monsterHealth != 0 && heroHealth != 0);
+
+           Console.WriteLine(monsterHealth>heroHealth? "Monster Wins!":"Hero Wins!");
+           
+        }
+    }
+
+
+}
