32bde19be7433e8e6da497eac9aef5fdce1ae833
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-04-16 16:26:29 +0530

    Generics Practice

---
+++ Generic/CacheSystemGenerics/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cache<int, string> dict = new Cache<int, string>(5);
+
+            dict.Add(1, "Ayush");
+            dict.Add(2, "Ayush");
+            dict.Add(3, "Ayush");
+            dict.Add(4, "Ayush");
+            dict.Add(5, "Ayush");
+
+            dict.Add(6, "Sanjyot"); // removes key = 1
+
+            dict.Display();
+        }
+    }
+
+    public class Cache<TKey, TValue> where TKey : notnull
+    {
+        private readonly int _capacity;
+        private readonly Dictionary<TKey, TValue> _cache;
+        private readonly List<TKey> _order;
+
+        public Cache(int capacity)
+        {
+            _capacity = capacity;
+            _cache = new Dictionary<TKey, TValue>();
+            _order = new List<TKey>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            
+            if (_cache.ContainsKey(key))
+            {
+                _cache[key] = value;
+                return;
+            }
+
+            
+            if (_cache.Count >= _capacity)
+            {
+                TKey oldestKey = _order[0];
+                _cache.Remove(oldestKey);
+                _order.RemoveAt(0);
+            }
+
+            _cache[key] = value;
+            _order.Add(key);
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            if (!_cache.ContainsKey(key))
+                throw new InvalidOperationException("Key not found");
+
+            return _cache[key];
+        }
+
+        public void Remove(TKey key)
+        {
+            if (!_cache.ContainsKey(key))
+                throw new InvalidOperationException("Key not found");
+
+            _cache.Remove(key);
+            _order.Remove(key);
+        }
+
+        public bool Exists(TKey key)
+        {
+            return _cache.ContainsKey(key);
+        }
+
+        public void Display()
+        {
+            foreach (var item in _cache)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+        }
+    }
+}
---
+++ Generic/StackGeneric/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            var list = new Stack<string>();
+            list.Push("Apple");
+            list.Push("Kiwi");
+            list.Push("Guava");
+            
+            Console.WriteLine("List is :");
+            list.DisplayStack();
+            Console.WriteLine("Adding new item to list : ");
+            list.Push("Banana");
+            list.DisplayStack();
+
+            Console.WriteLine("Using Peek : "+ list.Peek());
+            Console.WriteLine("Using Pop : "+ list.Pop());
+            list.DisplayStack();
+
+            Console.WriteLine("Clearing List ");
+            list.ClearStack();
+            list.DisplayStack();
+        }
+
+    }
+
+    public class Stack<T> 
+    {
+        private readonly List<T> _stack = new List<T>();
+
+        
+
+        public void Push(T item)
+        {
+            if(item == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            _stack.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            
+                var index = _stack.Count - 1;
+                var value = _stack[index];
+                _stack.RemoveAt(index);
+                return value;
+           
+
+        }
+
+        public T Peek()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return _stack[_stack.Count-1];
+
+        }
+
+        public void ClearStack()
+        {
+            _stack.Clear();
+        }
+
+        public void DisplayStack()
+        {
+            if (_stack.Count == 0)
+            {
+                Console.WriteLine("Stack us Empty");
+            }
+            foreach (var item in _stack) Console.WriteLine(item);
+
+        }
+    }
+}
