27d450ca81bf331b8b64d638aa4bbe866bc7d85b
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-04-16 16:25:51 +0530

    Generics Practice

---
+++ Generic/ArrayMaxGeneric/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = 10;
+            int[] arr = new int[size];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rand.Next(1, 100); 
+            }
+            Console.WriteLine("Array is :");
+            foreach (var item in arr)
+            {
+                Console.Write(item+", ");
+            }
+            Console.WriteLine("\nMax from the array is : " + ArrayMax(arr));
+        } 
+           
+    
+    
+
+    public static T ArrayMax<T>(T[] array) where T : IComparable
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentNullException("Array is Null");
+            }
+            T max = array[0];
+            foreach (var item in array)
+            {
+                if (item.CompareTo(max) > 0)
+                {
+                    max = item;
+                }
+
+            }
+            return max;
+
+        }
+    }
+}
