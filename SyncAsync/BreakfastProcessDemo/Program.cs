a31f66816e3a0993fc09f38e604e293222b7dd1c
ayush-sherigar-22 <ayushsherigar22@gmail.com>
2026-04-17 16:51:02 +0530

    Sync Asycn Practice

---
+++ SyncAsync/BreakfastProcessDemo/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Threading.Channels;
+
+namespace SyncAsync
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Sync
+            Console.WriteLine("Using Sync");
+            MakeToast();
+            MakeEggs();
+            MakeCoffee();
+            Console.WriteLine("Breakfast is ready..");
+
+            // Async
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Using Async");
+            Task toast = MakeToastAsync();
+            Task eggs = MakeEggsAsync();
+            
+
+            await Task.WhenAll(toast, eggs); // Run parallel
+            await MakeCoffeeAsync(); // Run individual
+            Console.WriteLine("Breakfast is ready..");
+
+        }
+        public static async Task MakeToastAsync()
+        {
+            Console.WriteLine("Started making Toast..");
+            await Task.Delay(3000);
+            Console.WriteLine("Toast is ready");
+        }
+
+        public static async Task MakeEggsAsync()
+        {
+            Console.WriteLine("Started making Eggs..");
+            await Task.Delay(4000);
+            Console.WriteLine("Eggs are ready"); 
+        }
+
+        public static async Task MakeCoffeeAsync()
+        {
+            Console.WriteLine("Started making Coffee..");
+            await Task.Delay(5000);
+            Console.WriteLine("Coffee is ready");
+        }
+
+
+
+        public static void MakeToast()
+        {
+            Console.WriteLine("Started making Toast..");
+            Thread.Sleep(3000);
+            Console.WriteLine("Toast is ready");
+        }
+
+        public static void MakeEggs()
+        {
+            Console.WriteLine("Started making Eggs..");
+            Thread.Sleep(4000);
+            Console.WriteLine("Eggs are ready");
+        }
+
+        public static void MakeCoffee()
+        {
+            Console.WriteLine("Started making Coffee..");
+            Thread.Sleep(5000);
+            Console.WriteLine("Coffee is ready");
+        }
+
+
+    }
+
+
+
+}
+
+
+
+
---
+++ SyncAsync/UserDashboardDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SyncAsync
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var user = new UserDashboard("Ayush", "Cake123");
+
+            var tasks = new Task<string>[]
+            {
+                user.GetProfile(),
+                user.GetOrders(),
+                user.GetNotification()
+            };
+
+            foreach (var task in tasks)
+            {
+                try
+                {
+                    var result = await task;
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("\nDashboard Loaded (Partial/Full)");
+        }
+    }
+
+    public class UserDashboard
+    {
+        private readonly string _userName;
+        private readonly string _orderId;
+
+        public UserDashboard(string username, string orderid)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(orderid))
+            {
+                throw new ArgumentNullException("Invalid Credentials");
+            }
+            _userName = username;
+            _orderId = orderid;
+        }
+
+        public async Task<string> GetProfile()
+        {
+            await Task.Delay(2000);
+            return $"Profile loaded for {_userName}";
+        }
+
+        public async Task<string> GetOrders()
+        {
+            await Task.Delay(4000);
+
+            // Simulate failure
+            //throw new Exception("Orders service failed!");
+
+            return $"Orders loaded for {_userName} with OrderID : {_orderId}";
+        }
+
+        public async Task<string> GetNotification()
+        {
+            await Task.Delay(1000);
+            return $"Notification ready for {_userName} with OrderID {_orderId}";
+        }
+    }
+}
